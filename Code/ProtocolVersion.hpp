123d9f354e98737377d3905dbf84fe04f0fda05f
Guillaume Puyal <contact@guillaume-puyal.com>
2017-10-06 21:04:39 +0200

    Added protocol version constant

---
+++ Code/ProtocolVersion.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+// Apoapse network protocol version number in the format [major][minor][revision] making one single integer with three digits.
+static constexpr int protocolVersion = 100;
