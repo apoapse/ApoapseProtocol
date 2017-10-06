e274ac008ec9c8cc348c816e157434b925c62558
Guillaume Puyal <contact@guillaume-puyal.com>
2017-10-06 21:05:12 +0200

    Added PBKDF2_SHA256 and PBKDF2_SHA512 helper hash functions

--- Code/Hash.hpp
+++ Code/Hash.hpp
@@ -5,6 +5,7 @@
 #include "HashTypes.hpp"
 
 #include <cryptopp/sha.h>
+#include <cryptopp/pwdbased.h>
 
 namespace Cryptography
 {
@@ -20,6 +21,26 @@ namespace Cryptography
 	{
 		std::array<byte, CryptoPP::SHA512::DIGESTSIZE> result;
 		CryptoPP::SHA512().CalculateDigest(result.data(), data.data(), data.size());
+
+		return result;
+	}
+
+	inline hash_SHA256 PBKDF2_SHA256(const std::vector<byte>& password, const std::vector<byte>& salt, UInt32 interationsCount)
+	{
+		std::array<byte, CryptoPP::SHA256::DIGESTSIZE> result;
+
+		CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA256> pbkdf2;
+		pbkdf2.DeriveKey(result.data(), result.size(), 0, password.data(), password.size(), salt.data(), salt.size(), interationsCount, 0);
+
+		return result;
+	}
+
+	inline hash_SHA512 PBKDF2_SHA512(const std::vector<byte>& password, const std::vector<byte>& salt, UInt32 interationsCount)
+	{
+		std::array<byte, CryptoPP::SHA512::DIGESTSIZE> result;
+
+		CryptoPP::PKCS5_PBKDF2_HMAC<CryptoPP::SHA512> pbkdf2;
+		pbkdf2.DeriveKey(result.data(), result.size(), 0, password.data(), password.size(), salt.data(), salt.size(), interationsCount, 0);
 
 		return result;
 	}
