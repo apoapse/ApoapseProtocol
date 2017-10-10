68565c41b2c4a4af9d49334becd8357fedb677a5
Guillaume Puyal <contact@guillaume-puyal.com>
2017-10-10 16:34:43 +0200

    Added hash length constants

--- Code/HashTypes.hpp
+++ Code/HashTypes.hpp
@@ -2,11 +2,15 @@
 #include "TypeDefs.hpp"
 #include <array>
 
+static constexpr UInt32 sha256Length = 32;
+static constexpr UInt32 sha384Length = 48;
+static constexpr UInt32 sha512Length = 64;
+
 // SHA-2
-using hash_SHA256 = std::array<byte, 32>;
-using hash_SHA512 = std::array<byte, 64>;
+using hash_SHA256 = std::array<byte, sha256Length>;
+using hash_SHA512 = std::array<byte, sha512Length>;
 
 // SHA-3
-using hash_SHA3_256 = std::array<byte, 32>;
-using hash_SHA3_384 = std::array<byte, 48>;
-using hash_SHA3_512 = std::array<byte, 64>;
+using hash_SHA3_256 = std::array<byte, sha256Length>;
+using hash_SHA3_384 = std::array<byte, sha384Length>;
+using hash_SHA3_512 = std::array<byte, sha512Length>;
