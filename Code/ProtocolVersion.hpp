aa8b762d0a9d325df4ab38cb342c42cf7974f7f2
Guillaume Puyal <contact@guillaume-puyal.com>
2018-07-27 01:16:11 +0200

    Cleanup before MVP work

--- Code/ProtocolVersion.hpp
+++ Code/ProtocolVersion.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-// Apoapse network protocol version number in the format [major][minor][revision] making one single integer with three digits.
-static constexpr int protocolVersion = 100;
+// Apoapse network protocol version number in the format [major][minor][minor][revision][revision] making one single integer with five digits. Example: 1.25.01 is 12501
+static constexpr int protocolVersion = 1'00'00;
