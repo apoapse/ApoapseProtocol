187d4f66fa4ce76a5da21ffc19451f554f4bb414
Guillaume Puyal <contact@guillaume-puyal.com>
2018-07-30 20:46:36 +0200

    Added basic operations system

---
+++ Code/Operation.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include "Operation.h"
+#include "Common.h"
+#include "SQLQuery.h"
+#include "DateTimeUtils.h"
+
+Operation::Operation(OperationType type, OperationDirection direction, const Username& user, std::optional<Uuid> itemUuid)
+	: type(type)
+	, direction(direction)
+	, user(user)
+	, itemUuid(itemUuid)
+{
+
+}
+
+void Operation::Save()
+{
+	if (m_dbId != -1)
+		throw std::exception("This operation is already saved");
+
+	m_dbId = GetPreviousDbId();
+	time = DateTimeUtils::UnixTimestampNow();
+
+	SQLQuery query(*global->database);
+	if (itemUuid.has_value())
+		query << INSERT_INTO << "operations" << " (id, type, direction, time, user, item_uuid)" << VALUES << "(" << m_dbId << "," << static_cast<Int32>(type) << "," << static_cast<Int8>(direction) << "," << time << "," << user.GetRaw() << "," << itemUuid.value().GetInRawFormat() << ")";
+ 	else
+ 		query << INSERT_INTO << "operations" << " (id, type, direction, time, user)" << VALUES << "(" << m_dbId << "," << static_cast<Int32>(type) << "," << static_cast<Int8>(direction) << "," << time << "," << user.GetRaw() << ")";
+
+	query.Exec();
+}
+
+DbId Operation::GetPreviousDbId()
+{
+	SQLQuery query(*global->database);
+	query << SELECT << "id" << FROM << "operations" << ORDER_BY << "id" << DESC << LIMIT << "1";
+	auto res = query.Exec();
+
+	if (res && res.RowCount() == 1)
+		return res[0][0].GetInt64();
+	else
+		return 0;
+}
---
+++ Code/Operation.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Database.hpp"
+#include "Uuid.h"
+#include "AbstractUser.h"
+#include <optional>
+#include "Username.h"
+
+enum class OperationType : UInt32
+{
+	none,
+	new_user
+};
+
+enum class OperationDirection : UInt8
+{
+	none,
+	sent,
+	received
+};
+
+class Operation
+{
+	DbId m_dbId = -1;
+
+public:
+	OperationType type = OperationType::none;
+	OperationDirection direction = OperationDirection::none;
+	Username user;
+	std::optional<Uuid> itemUuid;
+	Int64 time = -1;
+
+	Operation(OperationType type, OperationDirection direction, const Username& user, std::optional<Uuid> itemUuid);
+	void Save();
+	//virtual ~Operation();
+	
+private:
+	static DbId GetPreviousDbId();
+};
