c6786fbfb432e6dfda8b89c54b6f3c4aa55092e1
Guillaume Puyal <contact@guillaume-puyal.com>
2019-09-14 22:48:56 +0200

    Added server port n+1

--- Code/GlobalVarDefines.hpp
+++ Code/GlobalVarDefines.hpp
@@ -3,4 +3,4 @@
 // Apoapse network protocol version number in the format [major][minor][minor][revision][revision] making one single integer with five digits. Example: 1.25.01 is 12501
 static constexpr int protocolVersion = 1'01'00;
 static constexpr UInt16 defaultServerPort = 3000;
-static constexpr UInt16 defaultFileStreamPort = 3001;
+static constexpr UInt16 defaultFileStreamPort = defaultServerPort + 1;
