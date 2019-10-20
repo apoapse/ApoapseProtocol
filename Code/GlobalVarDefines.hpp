05c9b64c16650063cc63ca67a0a3d766364a62b6
Guillaume Puyal <contact@guillaume-puyal.com>
2019-10-20 23:09:29 +0200

    Bump protocol version

--- Code/GlobalVarDefines.hpp
+++ Code/GlobalVarDefines.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 // Apoapse network protocol version number in the format [major][minor][minor][revision][revision] making one single integer with five digits. Example: 1.25.01 is 12501
-static constexpr int protocolVersion = 1'01'00;
+static constexpr int protocolVersion = 1'01'01;
 static constexpr int serverVersion = 1'00'00;
 static constexpr UInt16 defaultServerPort = 3000;
 static constexpr UInt16 defaultFileStreamPort = defaultServerPort + 1;
